8ee6731c2b6cbab160d82fa0d38c9e5d80fe8605
Semilore317 <abraham.o.bankole@gmail.com>
2026-01-20 12:47:25 +0100

    Spring Initializr setup of project
    A basic spring project with the springboot dependencies added to the maven POM file. JDK 21 and Springboot 3  Database is PostgresSQL

---
+++ src/main/java/com/SkillsForge/Expense_Tracker_Backend_Java/ExpenseTrackerBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.SkillsForge.Expense_Tracker_Backend_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerBackendJavaApplication.class, args);
+	}
+
+}
