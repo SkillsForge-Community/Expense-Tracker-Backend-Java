0fe03d7f1a62e8502754d7bdaf707901df9bb2d1
owolabiezekiel <owo.ezekiel@gmail.com>
2026-01-26 16:52:01 +0100

    empty transaxtions

--- src/main/java/com/SkillsForge/expensetracker/ExpenseTrackerBackendJavaApplication.java
+++ src/main/java/com/SkillsForge/expensetracker/ExpenseTrackerBackendJavaApplication.java
@@ -1,4 +1,4 @@
-package com.SkillsForge.Expense_Tracker_Backend_Java;
+package com.SkillsForge.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/SkillsForge/expensetracker/persistence/entity/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.SkillsForge.expensetracker.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@SuperBuilder
+@AllArgsConstructor
+public class BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  protected Long id;
+
+  @Column(nullable = false)
+  LocalDateTime createdAt;
+  @Column(nullable = false)
+  LocalDateTime updatedAt;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    BaseEntity that = (BaseEntity) o;
+    return Objects.nonNull(id) ? id.equals(that.id) : id == that.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
