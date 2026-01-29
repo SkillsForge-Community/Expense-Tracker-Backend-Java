0e32ec5397471a5fbf5a26adc44ba5f73afa797a
﻿Abraham Bankole <abraham.o.bankole@gmail.com>
2026-01-29 10:55:58 +0100

    replaced former implementation of createTransaction to make use of the revised TransactionDto

--- src/main/java/com/SkillsForge/expensetracker/service/TransactionService.java
+++ src/main/java/com/SkillsForge/expensetracker/service/TransactionService.java
@@ -1,9 +1,11 @@
 package com.SkillsForge.expensetracker.service;
 
 import com.SkillsForge.expensetracker.dto.CreateTransactionRequest;
+import com.SkillsForge.expensetracker.dto.TransactionDto;
 import com.SkillsForge.expensetracker.persistence.entity.Transaction;
 
 public interface TransactionService {
-    Transaction createTransaction(CreateTransactionRequest request);
+    //Transaction createTransaction(CreateTransactionRequest request);
+    Transaction createTransaction(TransactionDto transactionDto);
     Transaction getTransactionById(Long id);
 }
