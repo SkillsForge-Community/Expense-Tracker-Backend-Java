c822c0b64a9a28a5b590733bd15a09e7b5f075a8
﻿Abraham Bankole <abraham.o.bankole@gmail.com>
2026-01-29 11:46:41 +0100

    successfully moved all business logic and logging from TransactionController.java to the appropriate service class

--- src/main/java/com/SkillsForge/expensetracker/controller/TransactionController.java
+++ src/main/java/com/SkillsForge/expensetracker/controller/TransactionController.java
@@ -1,7 +1,6 @@
 package com.SkillsForge.expensetracker.controller;
 
 import com.SkillsForge.expensetracker.dto.TransactionDto;
-import com.SkillsForge.expensetracker.persistence.entity.Transaction;
 import com.SkillsForge.expensetracker.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,16 +17,14 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping
-    public ResponseEntity<TransactionDto> createTransaction(
-            @RequestBody TransactionDto request) {
+    public ResponseEntity<TransactionDto> createTransaction(@RequestBody TransactionDto request) {
         TransactionDto response = transactionService.createTransaction(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TransactionDto> getTransactionById(@PathVariable Long id) {
-        Transaction transaction = transactionService.getTransactionById(id);
-        TransactionDto response = TransactionDto.fromEntity(transactionService.getTransactionById(id));
+        TransactionDto response = transactionService.getTransactionById(id);
         return ResponseEntity.ok(response);
     }
 }
