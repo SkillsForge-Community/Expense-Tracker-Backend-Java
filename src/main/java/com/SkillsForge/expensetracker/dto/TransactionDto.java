efe85c9c68de931bb9baf5ae06bd388b6843f546
Semilore317 <abraham.o.bankole@gmail.com>
2026-01-30 01:08:24 +0100

    removed validations on the TransactionDto record

--- src/main/java/com/SkillsForge/expensetracker/dto/TransactionDto.java
+++ src/main/java/com/SkillsForge/expensetracker/dto/TransactionDto.java
@@ -3,26 +3,14 @@ package com.SkillsForge.expensetracker.dto;
 import com.SkillsForge.expensetracker.app.enums.TransactionCategory;
 import com.SkillsForge.expensetracker.app.enums.TransactionType;
 import com.SkillsForge.expensetracker.persistence.entity.Transaction;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDate;
 
 public record TransactionDto(
-        @NotBlank(message = "Description is required")
         String description,
-
-        @NotBlank(message = "Transaction Type is Required")
         TransactionType type,
-
-        @NotBlank(message = "Category is required")
         TransactionCategory category,
-
-        @NotBlank(message = "Transaction date is required")
         LocalDate date,
-
-        @NotBlank(message = "Amount is required")
-        @Positive(message = "Amount must be a Positive Integer")
         Long amount
 ){
     public static TransactionDto fromEntity(Transaction transaction){
