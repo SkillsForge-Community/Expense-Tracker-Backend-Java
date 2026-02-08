4cd934f880dc94b42930a6d6eac9c61c5d1abfeb
Semilore317 <abraham.o.bankole@gmail.com>
2026-02-08 23:07:05 +0100

    added the UsernameAlreadyExistsException class

--- src/main/java/com/SkillsForge/expensetracker/exception/UsernameAlreadyExistsException.java
+++ src/main/java/com/SkillsForge/expensetracker/exception/UsernameAlreadyExistsException.java
@@ -1,11 +1,8 @@
 package com.SkillsForge.expensetracker.exception;
 
-/**
- * Exception thrown when a user attempts to register with a username that
- * already exists
- */
+/** Exception thrown when a user attempts to register with a username that already exists */
 public class UsernameAlreadyExistsException extends RuntimeException {
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
+  public UsernameAlreadyExistsException(String message) {
+    super(message);
+  }
 }
