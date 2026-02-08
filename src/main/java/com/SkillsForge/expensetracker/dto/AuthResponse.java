77ea0bdcf447acd2f4cdc5d488eb84e4c0f4ccc1
Semilore317 <abraham.o.bankole@gmail.com>
2026-02-08 20:30:03 +0100

    added AuthResponse DTO

---
+++ src/main/java/com/SkillsForge/expensetracker/dto/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.SkillsForge.expensetracker.dto;
+
+import com.SkillsForge.expensetracker.app.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AuthResponse {
+    // no need for validation, this is output
+    private String token;          // the JWT token string
+    private String type;           // token type, always "Bearer"
+    private Long id;
+    private String username;
+    private String email;
+    private Role role;            // USER or ADMIN
+}
