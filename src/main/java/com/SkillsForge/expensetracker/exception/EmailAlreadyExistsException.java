17399b82a6d48f79379ef9e37497071ef25c1980
Semilore317 <abraham.o.bankole@gmail.com>
2026-02-08 22:36:54 +0100

    added extra exception classes

---
+++ src/main/java/com/SkillsForge/expensetracker/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.SkillsForge.expensetracker.exception;
+
+/**
+ * Exception thrown when a user attempts to register with an email that already
+ * exists
+ */
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
